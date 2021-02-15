02df86bb47145c5b7ad85568e16ed8284672abaa
kadirovgm <kadirovgm@gmail.com>
2021-02-15 12:35:18 +0300

    Initial commit

---
+++ shifr_zameny.py
@@ -0,0 +1,65 @@
+import random
+
+
+alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНПРСТУФЧЦЧШЩЪЫЬЭЮЯ '
+
+
+def keygen(alphabet):
+    with open('key.txt', mode='w') as file:
+        key = ''.join(random.sample(alphabet, len(alphabet)))
+        file.write(key)
+    return key
+
+
+key = keygen(alphabet)
+
+
+def encrypt(alphabet, key):
+    k = key
+    d = dict()
+    res = ""
+    for i in range(len(alphabet)):
+        d[alphabet[i]] = k[i]
+    with open('file.txt') as f:
+        read_data = f.read().strip()
+        for i in range(len(read_data)-1):
+            res += d[read_data[i]]
+    with open('output_enc.txt', 'w') as o:
+        o.write(res)
+
+
+def decrypt(key, alphabet):
+    a = alphabet
+    d = dict()
+    res = ""
+    for i in range(len(key)):
+        d[key[i]] = a[i]
+    with open('output_enc.txt') as enc:
+        read_data = enc.read().strip()
+        for i in range(len(read_data)-1):
+            res += d[read_data[i]]
+    with open('output_dec.txt', 'w') as o:
+        o.write(res)
+
+
+# next code is for menu
+print(" -----------------------------")
+print("| Type 'encrypt' or 'decrypt' |")
+print("| Type 'back' for exit        |")
+print(" -----------------------------")
+temp = 0
+while temp < 10:
+    choise = input()
+    if choise == 'encrypt':
+        encrypt(alphabet, key)
+        temp += 1
+        print("File.txt encrypted! Check output_enc.txt")
+    elif choise == 'decrypt':
+        decrypt(key, alphabet)
+        temp += 1
+        print("output_enc.txt decrypted! Check output_dec.txt")
+    elif choise == 'back':
+        break
+    else:
+        print("Wrong, try again 'encrypt' or  'decrypt'")
+
