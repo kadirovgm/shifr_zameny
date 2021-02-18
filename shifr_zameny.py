8ec75713e268cbb29b42497f6f5fb18a5e3116b5
kadirovgm <kadirovgm@gmail.com>
2021-02-18 12:31:33 +0300

    Project is done!

--- shifr_zameny.py
+++ shifr_zameny.py
@@ -1,4 +1,5 @@
 import random
+import math
 from collections import Counter
 
 alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНПРСТУФЧЦЧШЩЪЫЬЭЮЯ '
@@ -12,6 +13,9 @@ def keygen(alphabet):
 
 
 key = keygen(alphabet)
+for i in range(len(alphabet)):
+    if alphabet[i] == key[i]:
+        key = keygen(alphabet)
 
 
 def encrypt(alphabet, key):
@@ -22,7 +26,7 @@ def encrypt(alphabet, key):
         d[alphabet[i]] = k[i]
     with open('file.txt', encoding="utf-8") as f:
         read_data = f.read().strip()
-        for i in range(len(read_data)-1):
+        for i in range(len(read_data) - 1):
             res += d[read_data[i]]
     with open('output_enc.txt', 'w', encoding="utf-8") as o:
         o.write(res)
@@ -36,7 +40,7 @@ def decrypt(key, alphabet):
         d[key[i]] = a[i]
     with open('output_enc.txt', encoding="utf-8") as enc:
         read_data = enc.read().strip()
-        for i in range(len(read_data)-1):
+        for i in range(len(read_data) - 1):
             res += d[read_data[i]]
     with open('output_dec.txt', 'w', encoding="utf-8") as o:
         o.write(res)
@@ -61,8 +65,6 @@ while temp < 10:
         break
     else:
         print("Wrong, try again 'encrypt' or  'decrypt'")
-
-
 ########################################################## 1 METHOD
 # decrypting teachers cryptographic text
 # collection.Counter ()
@@ -85,7 +87,6 @@ sorted_keys = sorted(result, key=result.get, reverse=True)
 for w in sorted_keys:
     sorted_result[w] = result[w]
 #########################################################
-
 fr_dict = {' ': 0.175, 'о': 0.089, 'е': 0.072, 'а': 0.062, 'и': 0.062, 'т': 0.053, 'н': 0.053, 'с': 0.045,
            'р': 0.040, 'в': 0.038, 'л': 0.035, 'к': 0.028, 'м': 0.026, 'д': 0.025, 'п': 0.023, 'у': 0.021,
            'я': 0.018, 'ы': 0.016, 'з': 0.016, 'ь': 0.014, 'ъ': 0.014, 'б': 0.014, 'г': 0.013, 'ч': 0.012,
@@ -107,6 +108,8 @@ spis2 = ''.join(fr_dict.keys())
 print("The freq analys like list:" + str(spis1))
 print("The reference like list:" + str(spis2))
 ######################################################## encryption attempt
+
+
 def decrypt_p(result, fr_dict):
     fr = fr_dict
     d = dict()
@@ -121,7 +124,30 @@ def decrypt_p(result, fr_dict):
         o.write(res)
 #######################################################
 
+
+def entropy():
+    h = 0
+    pi = dict()
+    result = {}
+    with open('example.txt', encoding="utf-8") as ex:
+        read_ex = ex.read().strip()
+        for keys in read_ex:
+            result[keys] = result.get(keys, 0) + 1
+        for k in result:
+            pi[k] = (result.get(k, 0)) / len(read_ex)
+    s = 0
+    for value in pi.values():
+        s += value
+    print("Sum of probability: " + str(s))
+    print("Probability of every symbol: ")
+    for value in pi.values():
+        if value > 0:
+            h += float(value) * math.log((1 / float(value)), 2)
+        print(value)
+    print("entropy is: " + str(h))
+
+
+entropy()
 res_join = ''.join(sorted_result.keys())
 fr_dict_join = ''.join(fr_dict_podgon.keys())
 decrypt_p(res_join, fr_dict_join)
-
